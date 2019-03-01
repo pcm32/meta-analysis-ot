560dbd6fb3bc913a020cddd41fc5e186cdda636e
Pablo Moreno <pablo.a.moreno@gmail.com>
2019-03-01 11:23:43 +0000

    Finalize support for multiple accessions in inspect

--- inspectContrasts.R
+++ inspectContrasts.R
@@ -23,32 +23,38 @@ option_list = list(
 
 opt <- parse_args(OptionParser(option_list = option_list))
 
-accession <- opt$accession
-
-#xmlConfigFilename<-paste0(opt$outdir,"/",accession,"-configuration.xml")
-xmlConfigFilename<-tempfile(fileext = "xml")
-download.file(url=paste0("ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/atlas/experiments/",accession,"/",accession,"-configuration.xml"), destfile = xmlConfigFilename)
-experimentConfig <- parseAtlasConfig( xmlConfigFilename )
-experimentType <- experimentConfig$experimentType
-print(paste0(accession, " is of type ", experimentType) )
-
-analytics <- experimentConfig$allAnalytics[[1]]
-expContrasts <- atlas_contrasts( analytics )
-expAssayGroups <- assay_groups( analytics )
-
-cat(paste('Accession',"C.ID", "Size", "C. Name", sep="\t"),"\n")
-
-for( expContrast in expContrasts ) {
-  # Get the sample sizes for the contrast at hand.
-    refAssayGroupID <- reference_assay_group_id( expContrast )
-    testAssayGroupID <- test_assay_group_id( expContrast )
-    
-    refAssayGroup <- expAssayGroups[[ refAssayGroupID ]]
-    testAssayGroup <- expAssayGroups[[ testAssayGroupID ]]
-    
-    referenceSamplesSize<-length(refAssayGroup@biological_replicates)
-    testSamplesSize<-length(testAssayGroup@biological_replicates)
-    
-    cat(paste(accession, expContrast@contrast_id, paste0(referenceSamplesSize,":",testSamplesSize), expContrast@contrast_name, sep="\t"), "\n")
+if( is.na(opt$datasets_file) ) {
+  accessions<-c(opt$accession)  
+} else {
+  fread(opt$datasets_file)->datasets
+  accessions<-datasets$accession
 }
+
+for( accession in accessions ) {
+  #xmlConfigFilename<-paste0(opt$outdir,"/",accession,"-configuration.xml")
+  xmlConfigFilename<-tempfile(fileext = "xml")
+  download.file(url=paste0("ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/atlas/experiments/",accession,"/",accession,"-configuration.xml"), destfile = xmlConfigFilename)
+  experimentConfig <- parseAtlasConfig( xmlConfigFilename )
+  experimentType <- experimentConfig$experimentType
+  print(paste0(accession, " is of type ", experimentType) )
+  
+  analytics <- experimentConfig$allAnalytics[[1]]
+  expContrasts <- atlas_contrasts( analytics )
+  expAssayGroups <- assay_groups( analytics )
+  
+  cat(paste('Accession',"C.ID", "Size", "C. Name", sep="\t"),"\n")
+  
+  for( expContrast in expContrasts ) {
+    # Get the sample sizes for the contrast at hand.
+      refAssayGroupID <- reference_assay_group_id( expContrast )
+      testAssayGroupID <- test_assay_group_id( expContrast )
       
+      refAssayGroup <- expAssayGroups[[ refAssayGroupID ]]
+      testAssayGroup <- expAssayGroups[[ testAssayGroupID ]]
+      
+      referenceSamplesSize<-length(refAssayGroup@biological_replicates)
+      testSamplesSize<-length(testAssayGroup@biological_replicates)
+      
+      cat(paste(accession, expContrast@contrast_id, paste0(referenceSamplesSize,":",testSamplesSize), expContrast@contrast_name, sep="\t"), "\n")
+  }
+}      
